5da80ab26728c7bdfa7242943b866aa8b2539d6b
César López Cortés <cesarlopezcortes@hotmail.com>
2022-08-31 22:32:56 -0500

    NavBar component behaivor done

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,55 @@
-/* jQuery on Load */
-$(function ($) {
-  /* DOM elements */
-  var $windows = $(window);
+/* On Load */
+(function ($) {
+  /* Dom Elements. */
+  var $window = $(window);
   var $body = $("body");
 
-  /* Play initial animations on page load */
-  $windows.on("load", function () {
+  /* Breakpoints. */
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: [null, "480px"],
+  });
+
+  /* Play initial animations on page load. */
+  $window.on("load", function () {
     window.setTimeout(function () {
       $body.removeClass("is-preload");
     }, 100);
   });
+
+  /* Touch Mode */
+  if (browser.mobile) $body.addClass("is-touch");
+
+  /* Dropdowns. */
+  $("#nav > ul").dropotron({
+    alignment: "right",
+    hideDelay: 350,
+  });
+
+  /* Title Bar. */
+  $(
+    '<div id="titleBar">' +
+      '<a href="#navPanel" class="toggle"></a>' +
+      '<span class="title">' +
+      $("#logo").html() +
+      "</span>" +
+      "</div>"
+  ).appendTo($body);
+
+  /* Panel. */
+  $('<div id="navPanel">' + "<nav>" + $("#nav").navList() + "</nav>" + "</div>")
+    .appendTo($body)
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: "left",
+      target: $body,
+      visibleClass: "navPanel-visible",
+    });
 })(jQuery);
